ac209f1fa40b0af6dcf0c23ca99b2e3880ad6931
Gavin Lusby <gplc22@umsystem.edu>
2025-04-14 14:29:13 -0600

    Your commit message

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
 import requests
 import matplotlib.pyplot as plt
-import pandas as pd  # <-- NEW: We'll use pandas to filter by date
+import pandas as pd 
 
 app = Flask(__name__)
 
-# Function to get stock data from Alpha Vantage
 def get_stock_data(symbol):
     api_key = 'HF09Q56KFM690UMA'
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}'
@@ -13,13 +12,11 @@ def get_stock_data(symbol):
     data = response.json()
     return data['Time Series (Daily)']
 
-# Function to plot stock data
 def plot_stock_data(data, chart_type, start_date, end_date):
     df = pd.DataFrame.from_dict(data, orient='index')
     df.index = pd.to_datetime(df.index)
-    df = df.sort_index()  # Sort by date ascending
+    df = df.sort_index() 
     
-    # Filter between start_date and end_date
     df = df[(df.index >= pd.to_datetime(start_date)) & (df.index <= pd.to_datetime(end_date))]
     
     dates = df.index
@@ -38,16 +35,16 @@ def plot_stock_data(data, chart_type, start_date, end_date):
     plt.xticks(rotation=45)
     plt.legend()
     plt.tight_layout()
-    plt.savefig('static/stock_chart.png')  # Save the chart in the static folder
-    plt.close()  # Close the plot to avoid overlap
+    plt.savefig('static/stock_chart.png')
+    plt.close()  
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         stock_symbol = request.form['symbol']
-        chart_type = request.form['chart_type']  # <-- NEW
-        start_date = request.form['start_date']  # <-- NEW
-        end_date = request.form['end_date']      # <-- NEW
+        chart_type = request.form['chart_type']  
+        start_date = request.form['start_date']  
+        end_date = request.form['end_date']     
 
         data = get_stock_data(stock_symbol)
         plot_stock_data(data, chart_type, start_date, end_date)
